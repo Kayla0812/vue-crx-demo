96cc43f1f5439fdf6c1eb208756e0ebde107dfbe
lijiali <lijiali@zingfront.com>
2022-01-18 16:05:31 +0800

    update badge
    to show browser_action and page_action

--- background.js
+++ background.js
@@ -4,5 +4,9 @@ let color = '#ecf5ff';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color: '#ecf5ff' });
-    console.log('Default background color set to %clightBlue', `color: ${color}`);
+  console.log('Default background color set to %clightBlue', `color: ${color}`);
+
+  // 设置徽章
+  // chrome.browserAction.setBadgeText({ text: 'ON' });
+  // chrome.browserAction.setBadgeBackgroundColor({ color: '#108eff' });
 });
